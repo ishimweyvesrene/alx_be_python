33d4eac43ecf0660297acc97934c0d71042688f2
ishimweyvesrene <126156147+ishimweyvesrene@users.noreply.github.com>
2025-10-12 01:55:00 +0200

    Add files via upload

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,27 @@
+# polymorphism_demo.py
+import math
+
+# Base Class
+class Shape:
+    def area(self):
+        # Abstract-like method — must be overridden in subclasses
+        raise NotImplementedError("Subclasses must implement the area() method")
+
+
+# Derived Class - Rectangle
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+
+# Derived Class - Circle
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
